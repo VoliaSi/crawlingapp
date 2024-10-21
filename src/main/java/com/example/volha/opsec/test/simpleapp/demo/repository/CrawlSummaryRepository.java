62d5e5e17c311f30d317a20f68ca292988bc5fd0
Volha <kresiko@gmail.com>
2024-10-21 15:16:57 +0300

    feat: test added.

---
+++ src/main/java/com/example/volha/opsec/test/simpleapp/demo/repository/CrawlSummaryRepository.java
@@ -0,0 +1,35 @@
+package com.example.volha.opsec.test.simpleapp.demo.repository;
+
+import com.example.volha.opsec.test.simpleapp.demo.data.CrawlSummary;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class CrawlSummaryRepository {
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    public void save(CrawlSummary crawlSummary) {
+        String sql = "INSERT INTO crawl_summary (url, internal_links, external_links, images, unknown_links) VALUES (?, ?, ?, ?, ?)";
+        jdbcTemplate.update(sql, crawlSummary.getUrl(), crawlSummary.getInternalLinks(),
+                crawlSummary.getExternalLinks(), crawlSummary.getImages(), crawlSummary.getUnknownLinks());
+    }
+
+    public List<CrawlSummary> findAll() {
+        String sql = "SELECT * FROM crawl_summary";
+        return jdbcTemplate.query(sql, (rs, rowNum) -> {
+            CrawlSummary summary = new CrawlSummary();
+            summary.setId(rs.getLong("id"));
+            summary.setUrl(rs.getString("url"));
+            summary.setInternalLinks(rs.getInt("internal_links"));
+            summary.setExternalLinks(rs.getInt("external_links"));
+            summary.setImages(rs.getInt("images"));
+            summary.setUnknownLinks(rs.getInt("unknown_links"));
+            return summary;
+        });
+    }
+}
